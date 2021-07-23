032987d355365b118e376be6b28630b107a51a66
Arinze Anozie <arinze.anozie@aetainvestments.com>
2021-07-23 04:16:21 -0400

    Add extension to use table prefix with Dynamo

---
+++ src/Aeta.Extensions.PocoDynamo/PocoDynamoExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.Aws.DynamoDb;
+
+namespace Aeta.Extensions.PocoDynamo
+{
+    public static class PocoDynamoExtensions
+    {
+        public static IPocoDynamo UseTablePrefix(this IPocoDynamo poco, string prefix)
+        {
+            foreach (var tableMetadata in DynamoMetadata.GetTables()) tableMetadata.Name = prefix + tableMetadata.Name;
+
+            return poco;
+        }
+    }
+}
